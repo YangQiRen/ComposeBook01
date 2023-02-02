e2dbeb0e57f2bcd07f05cbbc5f0763a24c8a31c5
Hulk <hulkyang@taipay.com>
2023-02-02 17:43:43 +0800

    [學習] 圖片點擊拖移旋轉，並且用Canvas畫出進度條 圖片使用onGloballyPositioned抓取點擊拖移位置算出角度，將角度換算成百分比

--- app/src/main/java/com/example/composebook01/MainActivity.kt
+++ app/src/main/java/com/example/composebook01/MainActivity.kt
@@ -3,21 +3,63 @@ package com.example.composebook01
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.example.composebook01.ui.CircularProgressBar
+import com.example.composebook01.ui.MusicKnob
+import com.example.composebook01.ui.VolumeBar
+import kotlin.math.cos
+import kotlin.math.roundToInt
+import kotlin.math.tan
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var volume by remember {
+                mutableStateOf(0f)
+            }
+            val barCount = 30
+
             Box(
                 contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF101010))
             ) {
-                CircularProgressBar(percentage = 0.8f, number = 100)
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .border(1.dp, Color.Green, RoundedCornerShape(10.dp))
+                        .padding(30.dp)
+                ) {
+                    MusicKnob(
+                        modifier = Modifier.size(100.dp),
+                        onValueChange = {
+                            volume = it
+                            println("=====volume=$volume")
+                        }
+                    )
+                    Spacer(modifier = Modifier.width(20.dp))
+                    VolumeBar(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(30.dp),
+                        activeBars = (barCount * volume).roundToInt(),
+                        barCount = barCount
+                    )
+                }
             }
         }
     }
