6b3d542e94e18acd15e98be5cb40c90c8a9d69dd
HulkYang <hulkyangqiren@gmail.com>
2023-02-09 14:03:55 +0800

    [新增] 使用 NestedScrollConnection 抓取LazyColume捲動 Y偏移量，使用偏移量做出多疊層動態圖

--- app/src/main/java/com/example/composebook01/MainActivity.kt
+++ app/src/main/java/com/example/composebook01/MainActivity.kt
@@ -9,16 +9,29 @@ import androidx.compose.animation.core.CubicBezierEasing
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -31,10 +44,100 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Surface(
-                color = Color(0xFF101010),
+//                color = Color(0xFF101010),
                 modifier = Modifier.fillMaxSize()
             ) {
-                Navigation()
+//                Navigation()
+                val moonScrollSpeed = 0.08f
+                val midBgScrollSpeed = 0.03f
+
+                val imageHeight = (LocalConfiguration.current.screenWidthDp * (2f / 3f)).dp
+                val lazyListState = rememberLazyListState()
+
+                var moonOffset by remember {
+                    mutableStateOf(0f)
+                }
+                var midBgOffset by remember {
+                    mutableStateOf(0f)
+                }
+                val nestedScrollConnection = object : NestedScrollConnection {
+                    override fun onPreScroll(
+                        available: Offset,
+                        source: NestedScrollSource
+                    ): Offset {
+                        val delta = available.y
+                        moonOffset += delta * moonScrollSpeed
+                        midBgOffset += delta * midBgScrollSpeed
+                        return super.onPreScroll(available, source)
+                    }
+                }
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .nestedScroll(nestedScrollConnection),
+                    state = lazyListState
+                ) {
+                    items(10) {
+                        Text(
+                            text = "Sample1 Item $it",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        )
+                    }
+                    item {
+                        Box(
+                            modifier = Modifier
+                                .clipToBounds()
+                                .fillMaxWidth()
+                                .height(imageHeight)
+                                .background(
+                                    Brush.verticalGradient(
+                                        listOf(
+                                            Color(0xFFf36b21),
+                                            Color(0xFFf9a521)
+                                        )
+                                    )
+                                )
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_moonbg),
+                                contentDescription = "moon",
+                                contentScale = ContentScale.FillWidth,
+                                alignment = Alignment.BottomCenter,
+                                modifier = Modifier.matchParentSize()
+                                    .graphicsLayer {
+                                        translationY = moonOffset
+                                    }
+                            )
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_midbg),
+                                contentDescription = "mid bg",
+                                contentScale = ContentScale.FillWidth,
+                                alignment = Alignment.BottomCenter,
+                                modifier = Modifier.matchParentSize()
+                                    .graphicsLayer {
+                                        translationY = midBgOffset
+                                    }
+                            )
+                            Image(
+                                painter = painterResource(id = R.drawable.ic_outerbg),
+                                contentDescription = "outer bg",
+                                contentScale = ContentScale.FillWidth,
+                                alignment = Alignment.BottomCenter,
+                                modifier = Modifier.matchParentSize()
+                            )
+                        }
+                    }
+                    items(20) {
+                        Text(
+                            text = "Sample2 Item $it",
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(16.dp)
+                        )
+                    }
+                }
             }
         }
     }
